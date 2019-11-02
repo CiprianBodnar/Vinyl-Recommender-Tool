2e3101c2584dac2319b7a348a966a3bf63f849bd
Ciprian <cyprian.bodnar@gmail.com>
2019-11-02 18:04:04 +0200

    added dependecies

--- vinyl-backend/lib/model/User.js
+++ vinyl-backend/lib/model/User.js
@@ -1,5 +1,5 @@
 "use strict"
-
+//https://appdividend.com/2017/12/21/simple-nodejs-authentication-system-using-passport/
 module.exports = class  User{
     constructor(name, email, userRole){
         this.name = name
