66c61d663634c96e2835e8bce9d94eef4604cbb1
Bodnar Ciprian <cyprian.bodnar@gmail.com>
2020-01-25 15:07:29 +0200

    Sparql results

--- vinyl-backend/lib/controller/sparql-api/sparql-controller.js
+++ vinyl-backend/lib/controller/sparql-api/sparql-controller.js
@@ -2,10 +2,21 @@ const router = require('express').Router();
 const auth = require('../../service/auth');
 const SPARQL = require('../../service/sparql_service')
 const my_sparql = new SPARQL();
-
+var request = require('request')
 
 router.get('/', auth.required, (req, res, next) => {
-    return res.json((my_sparql.myQuery("",""))
+    var result = my_sparql.myQuery("dani","moca") 
+    
+    var returnObj = request.get(result, function(error, response, body) {
+        var obj = JSON.parse(response.body)
+        var listOfBindings = obj['results']['bindings']
+        for(it in listOfBindings){
+            console.log(listOfBindings[it]['bandName']['value'] + "--" + listOfBindings[it]['members']['value'])
+        }
+        return listOfBindings
+     
+    });
+    return res.json((returnObj)
     );
 });
 
--- vinyl-backend/lib/service/sparql_service.js
+++ vinyl-backend/lib/service/sparql_service.js
@@ -1,4 +1,5 @@
 
+var rdfstore = require('rdfstore');
 
 class SPARQL_service {
     
@@ -12,10 +13,35 @@ myQuery(artist,title) {
             "?band foaf:name ?bandName",
             "FILTER(langMatches(lang(?bandName), \"en\"))",
            "}"].join(" ")
-        
+         
+
     var queryURL = DBP + "?query=" + encodeURIComponent(query) + "&format=json" ;
     return queryURL;
     }
-}
 
+rdf() {
+    rdfstore.create(function(err, store) {
+        store.execute('LOAD <http://dbpedia.org/resource/Tim_Berners-Lee> INTO GRAPH <http://example.org/people>', function() {
+      
+          store.setPrefix('dbp', 'http://dbpedia.org/resource/');
+      
+          store.node(store.rdf.resolve('dbp:Tim_Berners-Lee'),  "http://example.org/people", function(err, graph) {
+      
+            var peopleGraph = graph.filter(store.rdf.filters.type(store.rdf.resolve("foaf:Person")));
+      
+            store.execute('PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\
+                           PREFIX foaf: <http://xmlns.com/foaf/0.1/>\
+                           PREFIX : <http://example.org/>\
+                           SELECT ?s FROM NAMED :people { GRAPH ?g { ?s rdf:type foaf:Person } }',
+                           function(err, results) {
+      
+                             console.log(peopleGraph.toArray()[0].subject.valueOf() === results[0].s.value);
+      
+                           });
+          });
+      
+        });
+      });
+    }
+};
 module.exports = SPARQL_service
