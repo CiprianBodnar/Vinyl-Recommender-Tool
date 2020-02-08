57da5a84f93d538090d57a2736d2abd8ac648d26
Bodnar Ciprian <cyprian.bodnar@gmail.com>
2020-02-08 16:38:33 +0200

    Create endpoint to search a track

--- vinyl-backend/lib/service/sparql_service.js
+++ vinyl-backend/lib/service/sparql_service.js
@@ -3,7 +3,7 @@ var rdfstore = require('rdfstore');
 
 class SPARQL_service {
     
-myQuery(genre) {
+myGenreQuey(genre) {
     var DBP = "http://dbpedia.org/sparql";
     
     var query = [
@@ -15,35 +15,33 @@ myQuery(genre) {
             "FILTER(langMatches(lang(?bandName), \"en\"))",
            "}",
            "LIMIT 50",].join(" ")
-    console.log(query)
     var queryURL = DBP + "?query=" + encodeURIComponent(query) + "&format=json" ;
     return queryURL;
     }
 
-rdf() {
-    rdfstore.create(function(err, store) {
-        store.execute('LOAD <http://dbpedia.org/resource/Tim_Berners-Lee> INTO GRAPH <http://example.org/people>', function() {
+myArtistQuery(artist){
+
+  var DBP = "http://dbpedia.org/sparql";
+ 
+  var query = [
+    " SELECT * WHERE {",
+      "?song rdfs:label        ?label ;",
+      "      rdf:type   		    ?type ;",
+      "      dbo:musicalArtist ?artist ;",
+      "      owl:sameAs        ?same .",
       
-          store.setPrefix('dbp', 'http://dbpedia.org/resource/');
-      
-          store.node(store.rdf.resolve('dbp:Tim_Berners-Lee'),  "http://example.org/people", function(err, graph) {
-      
-            var peopleGraph = graph.filter(store.rdf.filters.type(store.rdf.resolve("foaf:Person")));
-      
-            store.execute('PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\
-                           PREFIX foaf: <http://xmlns.com/foaf/0.1/>\
-                           PREFIX : <http://example.org/>\
-                           SELECT ?s FROM NAMED :people { GRAPH ?g { ?s rdf:type foaf:Person } }',
-                           function(err, results) {
-      
-                             console.log(peopleGraph.toArray()[0].subject.valueOf() === results[0].s.value);
-      
-                           });
-          });
-      
-        });
-      });
-    }
+     " FILTER (?artist=<http://dbpedia.org/resource/Queen_(band)>)",
+     " FILTER (?type IN (dbo:MusicalWork))",
+     " FILTER (?type NOT IN (dbo:Album))",
+     " FILTER (regex(str(?same), 'wikidata.org/entity'))",
+     " FILTER (lang(?label) = 'en')",
+    "} LIMIT 100",].join(" ")
+    var queryURL = DBP + "?query=" + encodeURIComponent(query) + "&format=json" ;
+    return queryURL;
+
+  }
+
+ 
     getRandomInt(max) {
       return Math.floor(Math.random() * Math.floor(max));
     }
@@ -81,6 +79,7 @@ rdf() {
       }
       return mapOfBands
     }
+
 
     removeDuplicates(mapOfBands){
       var mappedResultList = []
